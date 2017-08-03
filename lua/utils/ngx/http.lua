bc7fe28a2fde584d2db770f1f9b5fab37d567df1
万鸿飞 <wanhongfei@bytedance.com>
2017-08-03 17:00:44 +0800

    lua post
    Change-Id: Ibc378dd02152db98026c70f65b1b7826ccb97dae

--- lua/utils/ngx/http.lua
+++ lua/utils/ngx/http.lua
@@ -10,4 +10,13 @@ function _M.get(url)
     return nil
 end
 
+-- post请求指定url，并获取response
+function _M.post(url,body)
+    local res = ngx.location.capture(url,{method=ngx.HTTP_POST, body=body})
+    if res.status == 200 then
+        return res.body
+    end
+    return nil
+end
+
 return _M
